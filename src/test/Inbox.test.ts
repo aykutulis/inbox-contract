38195499ed68386941125116507c2b622d88b2b1
Aykut Ulış <aykutulis@hotmail.com>
2022-03-26 02:41:30 +0300

    initial value testing

--- src/test/Inbox.test.ts
+++ src/test/Inbox.test.ts
@@ -3,6 +3,8 @@ import ganache from 'ganache-cli';
 import Web3 from 'web3';
 import { abi, evm } from '../compile';
 
+const INITIAL_MESSAGE = 'Hi there!';
+
 const web3 = new Web3(ganache.provider());
 
 let accounts: string[] = [];
@@ -11,12 +13,17 @@ let inbox: InstanceType<typeof web3.eth.Contract>;
 beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
   inbox = await new web3.eth.Contract(abi)
-    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+    .deploy({ data: evm.bytecode.object, arguments: [INITIAL_MESSAGE] })
     .send({ from: accounts[0], gas: 1000000 });
 });
 
 describe('Inbox', () => {
   it('deploys a contract', () => {
     assert.ok(inbox.options.address);
+  });
+
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, INITIAL_MESSAGE);
   });
 });
