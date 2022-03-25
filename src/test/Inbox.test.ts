ab00dce4c9fbc6c919766541c8dce389bcdfe910
Aykut Ulış <aykutulis@hotmail.com>
2022-03-25 05:30:30 +0300

    get accounts

--- src/test/Inbox.test.ts
+++ src/test/Inbox.test.ts
@@ -2,3 +2,15 @@ import assert from 'assert';
 import ganache from 'ganache-cli';
 import Web3 from 'web3';
 const web3 = new Web3(ganache.provider());
+
+let accounts: string[] = [];
+
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts();
+});
+
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+    console.log(accounts);
+  })
+})
