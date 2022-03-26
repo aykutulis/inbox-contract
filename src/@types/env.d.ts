645b185c3549ad165e8468886c8cfc442ec481dd
Aykut Ulış <aykutulis@hotmail.com>
2022-03-26 03:51:28 +0300

    deploy feature has been completed

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -2,5 +2,6 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     MNEMONIC_PHRASE?: string;
     RINKEBY_URI?: string;
+    INITIAL_CONTRACT_ARGS?: string;
   }
 }
--- src/deploy.ts
+++ src/deploy.ts
@@ -3,11 +3,39 @@ import HDWalletProvider from '@truffle/hdwallet-provider';
 import Web3 from 'web3';
 import { abi, evm } from './compile';
 
-const { MNEMONIC_PHRASE, RINKEBY_URI } = process.env;
+const { MNEMONIC_PHRASE, RINKEBY_URI, INITIAL_CONTRACT_ARGS } = process.env;
 
+// validate environment variables
 if (!MNEMONIC_PHRASE || !RINKEBY_URI) {
-  throw new Error('Please set the MNEMONIC_PHRASE and RINKEBY_URI environment variables');
+  throw new Error('Please set the "MNEMONIC_PHRASE" and "RINKEBY_URI" environment variables');
+}
+if (!INITIAL_CONTRACT_ARGS) {
+  throw new Error('Please set the "INITIAL_CONTRACT_ARGS" environment variable');
 }
 
+const initialContractArgs: string[] = (() => {
+  try {
+    return JSON.parse(INITIAL_CONTRACT_ARGS);
+  } catch (e) {
+    throw new Error('"INITIAL_CONTRACT_ARGS" must be a JSON array');
+  }
+})();
+
+// create a provider
 const provider = new HDWalletProvider(MNEMONIC_PHRASE, RINKEBY_URI);
 const web3 = new Web3(provider);
+
+// Deploy the contract
+(async () => {
+  const accounts = await web3.eth.getAccounts();
+  const account = accounts[0];
+
+  console.info('Attempting to deploy contract from account', account);
+
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: initialContractArgs })
+    .send({ from: account, gas: 1000000 });
+
+  console.info('Contract deployed to', result.options.address);
+  provider.engine.stop();
+})();
